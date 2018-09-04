0da6ec9c3a9ab5499b432690c6d3e1ce20294e35
Rumman Bin Ashraf <rumman.ashraf@gmail.com>
2018-09-04 23:21:24 +0600

    Added zonedDateTime to Instant, Conversion between LocalDate and MinguoDate

--- src/main/java/module/time/TimeConverter.java
+++ src/main/java/module/time/TimeConverter.java
@@ -1,8 +1,10 @@
 package module.time;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.chrono.MinguoDate;
 
 /**
  * @author rumman
@@ -12,5 +14,17 @@ public class TimeConverter {
 
     public static ZonedDateTime instantToZonedDateTime(Instant instant) {
         return instant.atZone(ZoneId.of("Asia/Tokyo"));
+    }
+
+    public static Instant zonedDateTimeToInstant(ZonedDateTime zonedDateTime) {
+        return zonedDateTime.toInstant();
+    }
+
+    public static MinguoDate getMinguoDate(LocalDate localDate) {
+        return MinguoDate.from(localDate);
+    }
+
+    public static LocalDate getLocalDate(MinguoDate minguoDate) {
+        return LocalDate.from(minguoDate);
     }
 }
--- src/test/java/module/time/TimeConverterTest.java
+++ src/test/java/module/time/TimeConverterTest.java
@@ -3,8 +3,11 @@ package module.time;
 import org.junit.Test;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.chrono.MinguoDate;
 
-import static module.time.TimeConverter.instantToZonedDateTime;
+import static module.time.TimeConverter.*;
 
 /**
  * @author rumman
@@ -14,6 +17,26 @@ public class TimeConverterTest {
 
     @Test
     public void testInstantToZonedDateTime() {
+        System.out.println(Instant.now());
         System.out.println(instantToZonedDateTime(Instant.now()));
+    }
+
+    @Test
+    public void testZonedDateTimeToInstant() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+        System.out.println(zonedDateTimeToInstant(zonedDateTime));
+    }
+
+    @Test
+    public void testGetMinguoDate() {
+        System.out.println(LocalDate.now());
+        System.out.println(getMinguoDate(LocalDate.now()));
+    }
+
+    @Test
+    public void testGetLocalDate() {
+        System.out.println(MinguoDate.now());
+        System.out.println(getLocalDate(MinguoDate.now()));
     }
 }
