1909b31acfb50b91a8995e71b030b06191d44cd8
Rumman Bin Ashraf <rumman.ashraf@gmail.com>
2018-10-29 10:54:20 +0600

    Added Create workbook with sheet, cell, row Examples

--- src/main/java/module/export/HelloWorkBook.java
+++ src/main/java/module/export/HelloWorkBook.java
@@ -1,11 +1,13 @@
 package module.export;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author rumman
@@ -13,7 +15,7 @@ import java.io.IOException;
  */
 public class HelloWorkBook {
 
-    public static void OpenWorkBook(String filePath) {
+    public static void openWorkBook(String filePath) {
         File file = new File(filePath);
         FileInputStream fIP = null;
         try {
@@ -34,5 +36,76 @@ public class HelloWorkBook {
         } else {
             System.out.println("Error to open openworkbook.xlsx file.");
         }
+    }
+
+    public static void createWorkBook(String filePath) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File(filePath));
+            workbook.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(String.format("{%s} written successfully", filePath));
+    }
+
+    public static void createWorkBookWithSheet(String filePath, String sheetName) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File(filePath));
+
+            workbook.createSheet(sheetName);
+
+            workbook.write(out);
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(String.format("{%s} written successfully with sheet {%s}", filePath, sheetName));
+    }
+
+    public static void createWorkBookWithSheetContainingRowCell(String filePath,
+                                                                String sheetName,
+                                                                Map<String, List<String>> workSheetContent) {
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File(filePath));
+
+            XSSFSheet sheet = workbook.createSheet(sheetName);
+
+            XSSFRow row = null;
+            int rowCount = 0;
+
+            for (String key: workSheetContent.keySet()) {
+                row = sheet.createRow(rowCount);
+
+                int cellCount = 0;
+
+                for (String cellContent: workSheetContent.get(key)) {
+                    Cell cell = row.createCell(cellCount);
+                    cell.setCellValue(cellContent);
+
+                    cellCount++;
+                }
+
+                rowCount++;
+            }
+
+            workbook.write(out);
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(String.format("{%s} written successfully with sheet {%s}", filePath, sheetName));
     }
 }
--- src/test/java/module/export/HelloWorkBookTester.java
+++ src/test/java/module/export/HelloWorkBookTester.java
@@ -2,6 +2,11 @@ package module.export;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author rumman
  * @since 10/22/18
@@ -9,7 +14,31 @@ import org.junit.Test;
 public class HelloWorkBookTester {
 
     @Test
+    public void openExcelTest(){
+        HelloWorkBook.openWorkBook("/home/rumman/test.xlsx");
+    }
+
+    @Test
     public void createExcelTest(){
-        HelloWorkBook.OpenWorkBook("/home/rumman/test.xlsx");
+        HelloWorkBook.createWorkBook("/home/rumman/testCreate.xlsx");
+    }
+
+    @Test
+    public void createExcelWithSheetNameTest(){
+        HelloWorkBook.createWorkBookWithSheet("/home/rumman/testCreate2.xlsx", "Test Sheet Name");
+    }
+
+    @Test
+    public void createExcelWithRowCellTest(){
+        Map<String, List<String>> workSheetContent = new TreeMap<>();
+
+        workSheetContent.put("One", Arrays.asList("Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text"));
+        workSheetContent.put("Two", Arrays.asList("Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text"));
+        workSheetContent.put("Three", Arrays.asList("Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text"));
+        workSheetContent.put("Four", Arrays.asList("Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text"));
+        workSheetContent.put("Five", Arrays.asList("Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text", "Dummy Text"));
+
+        HelloWorkBook.createWorkBookWithSheetContainingRowCell(
+                "/home/rumman/testCreate3.xlsx", "Test Sheet Name", workSheetContent);
     }
 }
