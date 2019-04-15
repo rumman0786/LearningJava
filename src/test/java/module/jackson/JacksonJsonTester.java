bdb2f5a125761fbb23fab4d2da8a4ec19028fbd2
Rumman Bin Ashraf <rumman.ashraf@gmail.com>
2019-04-15 11:34:20 +0600

    Added read and iterate json nodes example

--- src/test/java/module/jackson/JacksonJsonTester.java
+++ src/test/java/module/jackson/JacksonJsonTester.java
@@ -2,11 +2,13 @@ package module.jackson;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -15,6 +17,8 @@ import java.util.Map;
  */
 public class JacksonJsonTester {
     public static void main(String args[]){
+
+        JacksonJsonTester jsonTester = new JacksonJsonTester();
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
@@ -46,9 +50,37 @@ public class JacksonJsonTester {
 
             String jsonFromMap = mapper.writeValueAsString(otherValues);
             System.out.println(jsonFromMap);
+
+            jsonTester.iterateJsonNode();
         }
         catch (JsonParseException e) { e.printStackTrace();}
         catch (JsonMappingException e) { e.printStackTrace(); }
         catch (IOException e) { e.printStackTrace(); }
+    }
+
+    private void iterateJsonNode() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "{\"name\":\"Mahesh Kumar\",  \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
+        JsonNode rootNode = mapper.readTree(jsonString);
+
+        JsonNode nameNode = rootNode.path("name");
+        System.out.println("Name: "+ nameNode.textValue());
+
+        JsonNode ageNode = rootNode.path("age");
+        System.out.println("Age: " + ageNode.intValue());
+
+        JsonNode verifiedNode = rootNode.path("verified");
+        System.out.println("Verified: " + (verifiedNode.booleanValue() ? "Yes":"No"));
+
+        JsonNode marksNode = rootNode.path("marks");
+        Iterator<JsonNode> iterator = marksNode.elements();
+        System.out.print("Marks: [ ");
+
+        while (iterator.hasNext()) {
+            JsonNode marks = iterator.next();
+            System.out.print(marks.intValue() + " ");
+        }
+
+        System.out.println("]");
     }
 }
